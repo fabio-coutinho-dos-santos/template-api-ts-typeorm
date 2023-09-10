170b5b533904c3a643940e53a9f1441b7002c07b
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-09-10 20:50:38 -0300

    created entites and migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,10 +6,16 @@ const PORT = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DB_URL
+  url: process.env.DB_URL,
+  // synchronize: false,
+  // logging: false,
+  // autoLoadEntities: true,
   // host: process.env.BD_HOST,
   // port: PORT,
   // username: process.env.BD_USER,
   // password: process.env.BD_PASS,
   // database: process.env.BD_NAM,
+  // autoLoadEntities: true,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
 })
